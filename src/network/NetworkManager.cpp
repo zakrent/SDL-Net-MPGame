b0c33b00adca9a398422c854655b603ebe10175d
zakrent <szymon.zakrent@gmail.com>
2017-09-28 20:32:02 +0200

    Created NetworkManager class

---
+++ src/network/NetworkManager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by zakrent on 9/28/17.
+//
+
+#include "NetworkManager.h"
---
+++ src/network/NetworkManager.h
@@ -0,0 +1,14 @@
+//
+// Created by zakrent on 9/28/17.
+//
+
+#ifndef GAME_MP_SERVER_NETWORK_H
+#define GAME_MP_SERVER_NETWORK_H
+
+
+class Network {
+
+};
+
+
+#endif //GAME_MP_SERVER_NETWORK_H
